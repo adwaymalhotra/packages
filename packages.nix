a0b28c16051f0c7438e73b1f9c3a7386deddd0ba
Adway Malhotra <adway@google.com>
2025-01-29 18:12:22 +0000

    add hyprpicker and xwayland-satellite

--- packages.nix
+++ packages.nix
@@ -38,6 +38,7 @@ in {
 
     grim
     hypridle
+    hyprpicker
     # hyprpaper # segfaults on work system with or without nixGL.
     swww
     kanata
@@ -46,9 +47,9 @@ in {
     rofi-wayland
     slurp
     # swaylock # red screen after unlock; seems to be fixed at HEAD; build from source for now.
-    # swaylock-effects
     wireplumber
     waybar
+    xwayland-satellite
 
     # niri # (doesn't seem to work with the systemd hook in .config; seems to be some opengl issues)
     # can't seem to install using this path
