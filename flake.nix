d81815f2a97821cfd1bcd71080a031a1c517b2f8
Adway Malhotra <adway@google.com>
2025-04-24 16:57:04 +0100

    use ghostty nixpkg instead of the flake.

--- flake.nix
+++ flake.nix
@@ -5,14 +5,13 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     nixgl.url = "github:guibou/nixGL";
-    ghostty.url = "github:ghostty-org/ghostty";
   };
 
-  outputs = { self, nixpkgs, flake-utils, nixgl, ghostty }:
+  outputs = { self, nixpkgs, flake-utils, nixgl }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        packages = import ./packages.nix  { inherit pkgs; ghostty=ghostty; nixgl=nixgl; };
+        packages = import ./packages.nix  { inherit pkgs; nixgl=nixgl; };
       in {
         packages = {
           default = pkgs.buildEnv {
--- packages.nix
+++ packages.nix
@@ -1,4 +1,4 @@
-{ pkgs, ghostty, nixgl }:
+{ pkgs, nixgl }:
 
 let
   system = pkgs.system;
@@ -11,6 +11,7 @@ in {
     eza
     fd
     fzf
+    ghostty
     helix
     lutgen
     macchina
@@ -24,13 +25,6 @@ in {
     yazi
     zellij
     zoxide
-
-    ghostty.packages.${system}.ghostty
-
-    # toolchains
-    cargo
-    rustc
-    rust-analyzer
   ];
   linux = with pkgs; [
     easyeffects
@@ -60,3 +54,16 @@ in {
   ];
   darwin = with pkgs; [];
 }
+
+# Things that are not here:
+# niri
+# swaylock (mainly because we need a fix that's not available in the nixpkgs)
+# rustup (doesn't seem like there's a straightforward way to install this via nix, though I'm sure running a simple curl command is possible inside the flake)
+# nixgl is required for apps that use the nix display environment. this is probably not necessary outside of work. not sure if it's easy to install this in a flake.
+# neovim's beta versions don't seem to be available in nixpkgs
+
+# This flake is useful for two main reasons:
+# 1. This is a list packages that we require to be installed on most of our systems. We probably want these installed on a system if working on it interactively.
+# 2. On cloudtops it is cumbersome to install these packages manually. The alternative is to create this list in a normal text file and then use a script to install it at bootstrap but it's not necessarily straightforward to do that on Debian systems where the newest available dependencies may be too old.
+
+# There are downsides though. Nix represents a parallel set of libs and binaries to the installed Linux system. This means we are duplicating a lot of packages and using all that extra disk usage. And obviously, these need to be downloaded when setting up a computer.
