fc1f733fe81c3d78092ac30e2672865503c7a8d2
Adway Malhotra <adway@google.com>
2025-05-29 15:53:13 +0100

    Updates feat. nixGL integration
    Integrate nixGL directly to avoid having to prefix GUI apps with nixGL.
    
    Remove xwayland-satellite as that doesn't seem to work well inside the
    nix world.
    
    Update packages.

--- flake.nix
+++ flake.nix
@@ -11,14 +11,31 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        packages = import ./packages.nix  { inherit pkgs; nixgl=nixgl; };
-      in {
+        packages = import ./packages.nix { inherit pkgs; nixgl = nixgl; };
+        getName = guiApp:
+          if guiApp ? meta && guiApp.meta ? mainProgram then
+            guiApp.meta.mainProgram
+          else if guiApp ? pname then
+            guiApp.pname
+          else
+            guiApp.name;
+        wrapWithNixGL = guiApp:
+          let
+            appName = getName guiApp;
+          in
+          pkgs.writeShellScriptBin appName ''
+            exec ${nixgl.packages.${pkgs.system}.nixGLIntel}/bin/nixGLIntel ${guiApp}/bin/${appName} "$@"
+          '';
+        wrappedLinuxGuiPackages = map wrapWithNixGL packages.linux-gui;
+      in
+      {
         packages = {
           default = pkgs.buildEnv {
             name = "tools";
             paths = packages.common
-              ++ (if pkgs.stdenv.isLinux then packages.linux else [])
-              ++ (if pkgs.stdenv.isDarwin then packages.darwin else []);
+              ++ (if pkgs.stdenv.isLinux then packages.linux else [ ])
+              ++ (if pkgs.stdenv.isLinux then wrappedLinuxGuiPackages else [ ])
+              ++ (if pkgs.stdenv.isDarwin then packages.darwin else [ ]);
           };
         };
       }
--- packages.nix
+++ packages.nix
@@ -11,12 +11,10 @@ in {
     eza
     fd
     fzf
-    ghostty
     helix
     lutgen
     macchina
     nix-direnv
-    neovide
     neovim
     ripgrep
     ripgrep-all
@@ -32,38 +30,28 @@ in {
     foot.terminfo
 
     brightnessctl
-    grim
     hypridle
     hyprpicker
     imv
     kanata
     mako
-    nwg-look
-    pipewire
     rofi-wayland
-    slurp
     # swaylock # red screen after unlock; seems to be fixed at HEAD; build from source for now.
     swww
-    wireplumber
     waybar
-    xwayland-satellite
 
-    # niri # (doesn't seem to work with the systemd hook in .config; seems to be some opengl issues)
-    # can't seem to install using this path
-    # nixgl.packages.${system}.nixGL
+    nixgl.packages.${system}.nixGLIntel
+
+    # System services which are out of date on Debian
+    pipewire
+    wireplumber
   ];
-  darwin = with pkgs; [];
+  linux-gui = with pkgs; [
+    neovide
+    nwg-look
+    ghostty
+  ];
+  darwin = with pkgs; [
+    ghostty
+  ];
 }
-
-# Things that are not here:
-# niri
-# swaylock (mainly because we need a fix that's not available in the nixpkgs)
-# rustup (doesn't seem like there's a straightforward way to install this via nix, though I'm sure running a simple curl command is possible inside the flake)
-# nixgl is required for apps that use the nix display environment. this is probably not necessary outside of work. not sure if it's easy to install this in a flake.
-# neovim's beta versions don't seem to be available in nixpkgs
-
-# This flake is useful for two main reasons:
-# 1. This is a list packages that we require to be installed on most of our systems. We probably want these installed on a system if working on it interactively.
-# 2. On cloudtops it is cumbersome to install these packages manually. The alternative is to create this list in a normal text file and then use a script to install it at bootstrap but it's not necessarily straightforward to do that on Debian systems where the newest available dependencies may be too old.
-
-# There are downsides though. Nix represents a parallel set of libs and binaries to the installed Linux system. This means we are duplicating a lot of packages and using all that extra disk usage. And obviously, these need to be downloaded when setting up a computer.
